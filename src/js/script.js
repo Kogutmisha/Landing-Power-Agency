c2ba2634f5b090f05188172742f411fe09d52eb9
kogutmisha <koget.228@gmail.com>
2024-12-22 15:40:10 +0200

    feat section comment and trs_by finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,24 @@
 const body = document.querySelector('body');
+
+
+const swiper_2 = new Swiper ('.swiper_2' , {
+  spaceBetween: 10,
+  slidesPerView: 5,
+  freeMode: true,
+})
+
+const swiper_1 = new Swiper('.swiper_1', {
+  thumbs: {
+    swiper: swiper_2,
+    
+  },
+  });
+
+
+const swiper_3 = new Swiper('.swiper_3 ' , {
+  slidesPerView: 5,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+})
